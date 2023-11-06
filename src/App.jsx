25c9176d89ee9f800bbaeefc9191115056e90567
Sitch196 <sichinava1996@gmail.com>
2023-11-06 21:31:03 +0400

    vercel error

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,47 @@
-import Landing from "./Pages/Landing";
+import React, { useEffect } from "react";
 import { createGlobalStyle } from "styled-components";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Landing from "./Pages/Landing";
 import Register from "./Pages/Register";
 import Dashboard from "./Pages/Dashboard";
 import ApiPage from "./Pages/ApiPage";
-const GlobalStyle = createGlobalStyle`
-      *{
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        font-family: 'Roboto';
-      }
-    `;
 
-const App = () => (
-  <>
-    <div>
-      <GlobalStyle />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/dashboard/api" element={<ApiPage />} />
-      </Routes>
-    </div>
-  </>
-);
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto';
+  }
+`;
+
+const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Check if the user is logged in
+    const isLoggedIn =
+      localStorage.getItem("ImageUrl") && localStorage.getItem("name");
+
+    // If not logged in, redirect to the landing page
+    if (!isLoggedIn) {
+      navigate("/register");
+    }
+  }, []);
+
+  return (
+    <>
+      <div>
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard/api" element={<ApiPage />} />
+        </Routes>
+      </div>
+    </>
+  );
+};
 
 export default App;
